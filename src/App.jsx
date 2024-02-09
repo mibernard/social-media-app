aa76588424b4de623187aed6b3fce16cba2b5bf6
Matthew <matthewbernard4@gmail.com>
2024-02-08 20:36:14 -0800

    Update login and logout flow (generated by blackbox.ai )

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import AuthPage from './pages/AuthPage/AuthPage';
 import PageLayout from './Layouts/PageLayout/PageLayout';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import useAuthStore from './store/authStore';
 
 function App() {
+  const authUser = useAuthStore((state) => state.user);
   return (
     <PageLayout>
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='/auth' element={<AuthPage />}></Route>
+        <Route path='/' element={authUser ? <HomePage></HomePage> : <Navigate to={'/auth'}></Navigate>}></Route>
+        <Route path='/auth' element={!authUser ? <AuthPage></AuthPage> : <Navigate to={'/'}></Navigate>}></Route>
         <Route path='/:username' element={<ProfilePage />}></Route>
       </Routes>
     </PageLayout>
